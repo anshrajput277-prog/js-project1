00317fc35ad5f53042f6b1b9a7309ddb120cadbe
Ansh Rajput <ansh.rajput277@gmail.com>
2026-02-22 17:21:17 +0000

    Commit config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountID = 123456789;
+let accountName = "John Doe";
+var accountBalance = 1000.50;
+accountCity = "New York";
+
+console.log("Account ID:", accountID);
+console.table({
+    "Account Name": accountName,
+    "Account Balance": accountBalance,
+    "Account City": accountCity
+});
